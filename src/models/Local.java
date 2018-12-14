019a40b7d9f0926ab3843ae544f1a21cb9ea080b
momorg7 <morganathalita8@outlook.com>
2018-12-14 17:53:05 -0200

    Local modificado.

--- src/models/Local.java
+++ src/models/Local.java
@@ -1,6 +1,22 @@
 package models;
 
+public class Local
+{
 
-public class Local {
-
+	String nome;
+	
+	public Local(String nome)
+	{
+		this.nome = nome;
+	}
+	
+	public String getNome()
+	{
+		return nome;
+	}
+	
+	public void setNome(String Nome)
+	{
+		this.nome = nome;
+	}
 }
