1f429807c794dbb326f879309ad50803c5a78a36
momorg7 <morganathalita8@outlook.com>
2018-12-15 10:44:00 -0200

    Classe Cliente alterada

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,35 +1,9 @@
 package models;
 
-import java.util.ArrayList;
-import models.Passagem;
-import models.Pessoa;
-
-public class Cliente extends Pessoa {
-	
-	// private ArrayList<Passagem> passagens = new ArrayList<Passagem>();
-	
-	public Cliente(String nome, int cpf) {
+public class Cliente extends Pessoa
+{
+	public Cliente(String nome, int cpf)
+	{
 		super(nome, cpf);
-	}
-
-	public void comprarPassagem(String local) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void consultarVoos() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void cancelarPassagem(int indice) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void listarPassagens() {
-		// TODO Auto-generated method stub
-		
-	}
-	
+	}	
 }
